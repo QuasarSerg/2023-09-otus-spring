a5f6428d008079bb6110cb95a631e7594a0e70dc
QuasarSerg <quasar.se@gmail.com>
2023-11-08 23:00:29 +0300

    modified based on comments

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,6 +1,5 @@
 package ru.otus.hw.dao;
 
-import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
@@ -8,30 +7,46 @@ import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+    private static final char SEPARATOR = '|';
+
     private final TestFileNameProvider fileNameProvider;
+
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        try {
-            FileReader fileReader = new FileReader(fileNameProvider.getTestFileName());
-            CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(fileReader)
-                    .withType(QuestionDto.class)
-                    .withSeparator('|').build();
-            return csvToBean.parse().stream()
-                    .map(QuestionDto::toDomainObject)
-                    .collect(Collectors.toList());
-        } catch (FileNotFoundException e) {
-            throw new QuestionReadException("Failed to read file!", e);
+        return getListQuestionDto().stream()
+                .map(QuestionDto::toDomainObject)
+                .collect(Collectors.toList());
+    }
+
+    private List<QuestionDto> getListQuestionDto() {
+        if (fileNameProvider.getTestFileName().isEmpty()) {
+            return new ArrayList<>();
+        }
+        ClassLoader classLoader = getClass().getClassLoader();
+        try (InputStream inputStream = classLoader.getResourceAsStream(fileNameProvider.getTestFileName())) {
+            if (inputStream != null) {
+                try (InputStreamReader inputStreamReader = new InputStreamReader(inputStream)) {
+                    return new CsvToBeanBuilder<QuestionDto>(inputStreamReader)
+                            .withType(QuestionDto.class)
+                            .withSeparator(SEPARATOR).build()
+                            .parse();
+                }
+            } else {
+                String msg = String.format("The resource %s could not be found", fileNameProvider.getTestFileName());
+                throw new NullPointerException(msg);
+            }
+        } catch (IOException | NullPointerException e) {
+            throw new QuestionReadException("Failed to read file", e);
         }
     }
 }
--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,7 +1,7 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.dao.QuestionDao;
 import ru.otus.hw.domain.Answer;
 import ru.otus.hw.domain.Question;
 
@@ -13,14 +13,13 @@ public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
 
-    private final CsvQuestionDao csvQuestionDao;
+    private final QuestionDao csvQuestionDao;
 
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
         List<Question> all = csvQuestionDao.findAll();
         for (Question question: all) {
             String answers = question.answers().stream()
