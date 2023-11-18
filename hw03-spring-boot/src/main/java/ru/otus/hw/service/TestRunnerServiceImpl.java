59d91eac8713575d65bc7107f43a8fc8d031901c
QuasarSerg <quasar.se@gmail.com>
2023-11-18 21:57:53 +0300

    hw03

---
+++ hw03-spring-boot/src/main/java/ru/otus/hw/service/TestRunnerServiceImpl.java
@@ -0,0 +1,33 @@
+package ru.otus.hw.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+@Service
+@RequiredArgsConstructor
+public class TestRunnerServiceImpl implements CommandLineRunner {
+
+    private final TestService testService;
+
+    private final StudentService studentService;
+
+    private final ResultService resultService;
+
+    private final LocalizedIOServiceImpl localizedIOService;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            var student = studentService.determineCurrentStudent();
+            var testResult = testService.executeTestFor(student);
+            resultService.showResult(testResult);
+        } catch (QuestionReadException e) {
+            localizedIOService.printLine("Error reading questions");
+        } catch (Throwable e) {
+            localizedIOService.printLine("Unknown error");
+        }
+    }
+}
