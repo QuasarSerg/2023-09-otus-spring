baae66f809895b90e624b63462adf8f81f932656
QuasarSerg <quasar.se@gmail.com>
2024-02-22 20:58:59 +0300

    import jakarta.persistence.* excluded

--- hw07-jpa/src/main/java/ru/otus/hw/models/Comment.java
+++ hw07-jpa/src/main/java/ru/otus/hw/models/Comment.java
@@ -1,6 +1,14 @@
 package ru.otus.hw.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.AllArgsConstructor;
